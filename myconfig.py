50a115666e13bf5852c1fb99cd2b3ea529959a53
donkey <1328032567@qq.com>
2024-06-21 16:42:27 +0800

    enough version1.1

--- myconfig.py
+++ myconfig.py
@@ -147,7 +147,7 @@ PWM_STEERING_THROTTLE = {
     "PWM_THROTTLE_SCALE": 1.0,              # used to compensate for PWM frequence differences from 60hz; NOT for increasing/limiting speed
     "PWM_THROTTLE_INVERTED": False,         # True if hardware requires an inverted PWM pulse
     "STEERING_LEFT_PWM": 460,               #pwm value for full left steering
-    "STEERING_RIGHT_PWM": 296,              #pwm value for full right steering
+    "STEERING_RIGHT_PWM": 303,              #pwm value for full right steering
     "THROTTLE_FORWARD_PWM": 500,            #pwm value for max forward throttle
     "THROTTLE_STOPPED_PWM": 370,            #pwm value for no movement
     "THROTTLE_REVERSE_PWM": 220,            #pwm value for max reverse throttle
@@ -158,7 +158,7 @@ PWM_STEERING_THROTTLE = {
 #
 STEERING_CHANNEL = 1            #(deprecated) channel on the 9685 pwm board 0-15
 STEERING_LEFT_PWM = 460         #pwm value for full left steering
-STEERING_RIGHT_PWM = 296       #pwm value for full right steering
+STEERING_RIGHT_PWM = 303       #pwm value for full right steering
 THROTTLE_CHANNEL = 0            #(deprecated) channel on the 9685 pwm board 0-15
 THROTTLE_FORWARD_PWM = 500      #pwm value for max forward throttle
 THROTTLE_STOPPED_PWM = 370      #pwm value for no movement
